2aab4543abcbb496f2664577126609b54d9fb7b9
Max Hammerer <162450722+KupferKopf@users.noreply.github.com>
2025-07-05 01:01:48 +0200

    Add files via upload

---
+++ EP_2/2.Test_Vorbereitung/Own_code/Test_Ueben/src/Blob.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Blob {
+
+
+    private int i;
+
+    Set<Integer> test;
+
+ Map<Integer,String> test2;
+
+    LinkedList<Integer> tast;
+
+    ArrayDeque<Integer> te;
+}
---
+++ EP_2/2.Test_Vorbereitung/Own_code/Test_Ueben/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+
+    private int test;
+
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+
+        }
+    }
+}
